617e5f0ef9158c3f9482dfe2db0de118d5990ac2
Gianfranco Montoya <gms122@gmail.com>
2014-05-27 09:35:25 -0500

    test belcorp

--- system/expressionengine/third_party/authlms/pi.authlms.php
+++ system/expressionengine/third_party/authlms/pi.authlms.php
@@ -65,7 +65,7 @@ class Authlms
     public function auth_lms_first(){
         include_once 'nusoap/lib/nusoap.php';
         //$username= ee()->TMPL->fetch_param('username');
-        $username= "techvillaran";
+        $username= "pechvillaran";
         $id_curse= ee()->TMPL->fetch_param('id_curse');
         //instantiate the NuSOAP class and define the web service URL:
         $client = new nusoap_client('http://54.243.186.233/moodle/auth/belcorpws/belcorpws_server.php?wsdl', 'WSDL');
@@ -84,8 +84,9 @@ class Authlms
           print_r($client->getDebug());
           die();
         }
-        //$url='http://54.243.186.233/moodle/auth/belcorpws/client/client.php?usuario='.$username.'&token='.$answer.'&curso='.$id_curse;
-        $url='http://54.243.186.233/moodle/auth/belcorpws/client/client.php?usuario='.$username.'&token=43221747&curso='.$id_curse;
+        //$url = 'http://54.243.186.233/moodle/auth/belcorpws/client/client.php usuario=pechvillaran&token=43221747&curso=24'
+        $url='http://54.243.186.233/moodle/auth/belcorpws/client/client.php?usuario='.$username.'&token='.$answer.'&curso='.$id_curse;
+        //$url='http://54.243.186.233/moodle/auth/belcorpws/client/client.php?usuario='.$username.'&token=43221747&curso='.$id_curse;
         //output the response (in the form of a multidimensional array) from the function call:
         return '{exp:redirecturl url="'.$url.'"}';
         //header('Location: http://54.243.186.233/moodle/auth/belcorpws/client/client.php?usuario=peppinedo&token=ABCD&curso=24' );
