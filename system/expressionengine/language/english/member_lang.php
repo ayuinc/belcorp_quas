6e0f8a435cfea8e984565339e94e0f6dc606ee39
info@ayuinc.com <info@ayuinc.com>
2014-09-11 17:10:25 -0400

    Cambios en el index page

--- system/expressionengine/language/english/member_lang.php
+++ system/expressionengine/language/english/member_lang.php
@@ -75,7 +75,7 @@ $lang = array(
 'Debe presentar todos los campos',
 
 'invalid_existing_un_pw' =>
-'El nombre de usuario y / o contraseña existente que ha enviado no son válidos',
+'El nombre de usuario y / o contraseña existente que ha enviado no son válidos. Si olvidaste o no conoces tu usuario y /o contraseña puedes llamarnos al 4444 o enviar un email a <a href="mailto:soporteucb@belcorp.biz">soporteucb@belcorp.biz</a>',
 
 'unpw_updated' =>
 'Su configuración ha sido actualizado. Ahora puede acceder al sistema.',
