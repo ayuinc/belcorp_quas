3adc56fc443a939a4e01379be63c189df7a3c682
ayu <info@ayuinc.com>
2015-02-10 17:10:09 -0500

    Menu izquierdo integration

--- system/expressionengine/third_party/news/pi.news.php
+++ system/expressionengine/third_party/news/pi.news.php
@@ -40,10 +40,49 @@ class News
     // END
     
     public function entries_by_preferences() {
-	    $preferences = $this->preferences();
-	    $preferences = split(',', $preferences);
+	    $preferences = $this->get_user_preferences();
+	    $preferences = explode(',', $preferences);
 	    
-	    $query_categories = ee()->db->select('cat_id')->get_where();
+	    $q_categories_names = ee()->db->select('cat_name')->where_in('cat_id', $preferences)->get('exp_categories');
+	    $categories_names = array();
+	    
+	    foreach($q_categories_names->result() as $row) {
+		    array_push($categories_names, $row->cat_name);
+	    }
+	    			    
+	    $q_entries_id = ee()->db->where_in('cat_id', $preferences)->get('exp_category_posts');
+	    $entries_id = array();
+	    foreach ($q_entries_id->result() as $row)
+		{
+			array_push($entries_id, $row->entry_id);
+		}
+		
+		print_r($categories_names);
+				
+		$q_entries_data = ee()->db
+								->select('exp_channel_data.entry_id, title, field_id_85, field_id_86')
+								->join('exp_channel_titles', 'exp_channel_data.entry_id = exp_channel_titles.entry_id')
+								->where_in('exp_channel_data.entry_id', $entries_id)
+								->or_where_in('exp_channel_data.field_id_86', $categories_names)
+								->get('exp_channel_data');
+				
+		$variables = array();
+
+		foreach ($q_entries_data->result() as $row)
+		{	
+		    $variable_row = array(
+		        'title'  => $row->title,
+		        'noticias_url'    => $row->field_id_85,
+		        'noticias_categoria_principal' => $row->field_id_86,
+		        'noticias_otras_categorias' => $this->get_other_tags($row->entry_id)
+		    );
+		
+		    $variables[] = $variable_row;
+		}
+		
+		print_r($variables);
+		
+		return ee()->TMPL->parse_variables(ee()->TMPL->tagdata, $variables);
     }
     
     public function entries_by_vp() {
@@ -54,12 +93,37 @@ class News
 	    
     }
     
-    public function preferences() {
+    private function get_other_tags($entry_id) {
+	    $q_categories_names = ee()->db
+	    							->select('cat_name')
+									->join('exp_categories', 'exp_categories.cat_id = exp_category_posts.cat_id')
+									->where('exp_category_posts.entry_id', $entry_id)
+									->get('exp_category_posts');
+	    
+		$cats = array();
+	    foreach($q_categories_names->result() as $row) {
+		    $cats[] = $row->cat_name;
+	    }
+	    
+	    return $cats;
+    }
+    
+    private function get_user_preferences() {
 	    $member_id = ee()->session->userdata('member_id');
-	    $query_preferences = ee()->db->select('m_field_id_2')->get_where('exp_member_data', array('member_id' => $member_id));
+	    $query_preferences = ee()->db
+	    							->select('m_field_id_2')
+	    							->get_where('exp_member_data', array('member_id' => $member_id));
         $preferences = $query_preferences->result();
         $res = $preferences[0]->m_field_id_2;
         
         return $res;
+    }
+    
+    private function get_country() {
+	    
+    }
+    
+    private function get_vp() {
+	    
     }
 }
