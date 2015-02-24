dbec4508be1d606246b7277e8ec5cabd2ece3318
Gus <yo@soygus.com>
2015-02-24 11:44:57 -0500

    Layout control

--- n_js/ayu-off-canvas.js
+++ n_js/ayu-off-canvas.js
@@ -1,47 +1,61 @@
-// AYU OFF CANVAS 
+// // AYU OFF CANVAS 
 
-(function() {
+$(document).ready(function(){
+  var siteWrapper = $('#site-wrapper'),
+      siteMenu    = $('#site-menu'),
+      siteCanvas  = $('#site-canvas');
 
-    if(!WURFL.is_mobile) {
-      $('#site-menu').addClass("toggle-nav");
+  siteMenu.hover(function(){
+    if (!siteWrapper.hasClass('homepage-on')) {
+      siteWrapper.addClass('show-nav');
     }
+  }, function(){
+    siteWrapper.removeClass('show-nav');
+  });
+});
 
-    $(document).ready(function(){
-      var $menuToggle = $('.toggle-nav'),
-          $hoverToggle =  $('.hover-toggle'),
-          $linkToggle = $('.toggle-nav-link');
-      $linkToggle.on('click touchstart', toggleNav);
-      $menuToggle.on('mouseenter', showNav);
-      $menuToggle.on('mouseleave', hideNav);
-      $hoverToggle.on('mouseenter', hovered);
-      $hoverToggle.on('mouseleave', unhovered);
-    });
+// (function() {
 
-})();
+//     if(!WURFL.is_mobile) {
+//       $('#site-menu').addClass("toggle-nav");
+//     }
+
+//     $(document).ready(function(){
+//       var $menuToggle = $('.toggle-nav'),
+//           $hoverToggle =  $('.hover-toggle'),
+//           $linkToggle = $('.toggle-nav-link');
+//       $linkToggle.on('click touchstart', toggleNav);
+//       $menuToggle.on('mouseenter', showNav);
+//       $menuToggle.on('mouseleave', hideNav);
+//       $hoverToggle.on('mouseenter', hovered);
+//       $hoverToggle.on('mouseleave', unhovered);
+//     });
+
+// })();
 
 
-/*========================================
-=            CUSTOM FUNCTIONS            =
-========================================*/
-function toggleNav(e) {
-  e.preventDefault();
-  $('#site-wrapper').toggleClass('show-nav');
-  $('#site-canvas').toggleClass('opened');
-}
+// ========================================
+// =            CUSTOM FUNCTIONS            =
+// ========================================
+// function toggleNav(e) {
+//   e.preventDefault();
+//   $('#site-wrapper').toggleClass('show-nav');
+//   $('#site-canvas').toggleClass('opened');
+// }
 
-function showNav() {
-  $('#site-wrapper').addClass('show-nav');
-  $('#site-canvas').addClass('opened');
-}
+// function showNav() {
+//   $('#site-wrapper').addClass('show-nav');
+//   $('#site-canvas').addClass('opened');
+// }
 
-function hideNav() {
-  $('#site-wrapper').removeClass('show-nav');   
-}
+// function hideNav() {
+//   $('#site-wrapper').removeClass('show-nav');   
+// }
 
-function hovered(){
-  $(this).addClass('hovered');
-}
+// function hovered(){
+//   $(this).addClass('hovered');
+// }
 
-function unhovered(){
-  $(this).removeClass('hovered');
-}
+// function unhovered(){
+//   $(this).removeClass('hovered');
+// }
