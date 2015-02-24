e6e5117481bfcb7573b2e350f92adf9bf573b805
Gus <yo@soygus.com>
2015-02-24 12:04:21 -0500

    Footer

--- n_js/ayu-off-canvas.js
+++ n_js/ayu-off-canvas.js
@@ -1,5 +1,4 @@
 // // AYU OFF CANVAS 
-
 $(document).ready(function(){
   var siteWrapper = $('#site-wrapper'),
       siteMenu    = $('#site-menu'),
@@ -13,49 +12,3 @@ $(document).ready(function(){
     siteWrapper.removeClass('show-nav');
   });
 });
-
-// (function() {
-
-//     if(!WURFL.is_mobile) {
-//       $('#site-menu').addClass("toggle-nav");
-//     }
-
-//     $(document).ready(function(){
-//       var $menuToggle = $('.toggle-nav'),
-//           $hoverToggle =  $('.hover-toggle'),
-//           $linkToggle = $('.toggle-nav-link');
-//       $linkToggle.on('click touchstart', toggleNav);
-//       $menuToggle.on('mouseenter', showNav);
-//       $menuToggle.on('mouseleave', hideNav);
-//       $hoverToggle.on('mouseenter', hovered);
-//       $hoverToggle.on('mouseleave', unhovered);
-//     });
-
-// })();
-
-
-// ========================================
-// =            CUSTOM FUNCTIONS            =
-// ========================================
-// function toggleNav(e) {
-//   e.preventDefault();
-//   $('#site-wrapper').toggleClass('show-nav');
-//   $('#site-canvas').toggleClass('opened');
-// }
-
-// function showNav() {
-//   $('#site-wrapper').addClass('show-nav');
-//   $('#site-canvas').addClass('opened');
-// }
-
-// function hideNav() {
-//   $('#site-wrapper').removeClass('show-nav');   
-// }
-
-// function hovered(){
-//   $(this).addClass('hovered');
-// }
-
-// function unhovered(){
-//   $(this).removeClass('hovered');
-// }
